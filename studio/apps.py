41eafadb0689b8c4a7eccc4fbc57fe16dbe56e7f
Arielzamler <ariel.zamlerr@gmail.com>
2021-12-04 12:20:06 +0200

    aaaa

--- studio/apps.py
+++ studio/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MySiteConfig(AppConfig):
+class StudioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_site'
+    name = 'studio'
