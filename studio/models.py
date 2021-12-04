c7fc67f97c282c804b74fd712787cb8d8bdc37e4
Matan Tsour <matani.ts1@gmail.com>
2021-12-04 16:04:15 +0200

    models wout migrations

--- studio/models.py
+++ studio/models.py
@@ -29,9 +29,6 @@ class User(models.Model):
     dor = models.DateField(max_length=8)  # date of registration
     organization = models.CharField(max_length=100)
 
-    def __str__(self):
-        return "-".join([self.fname+" "+self.lname, self.id, self.user_type])
-
 
 class Employee(models.Model):
     u_id = models.OneToOneField(
@@ -40,8 +37,18 @@ class Employee(models.Model):
         primary_key=True,
     )
     title = models.CharField(max_length=100)
-    wage_method = models.IntegerChoices(
-        'Hourly', 'Per_Project', 'Dynamic', 'Monthly', 'Unknown')
+    WAGE_METHOD_CHOICES = [
+        ('1', 'Hourly'),
+        ('2', 'Monthly'),
+        ('3', 'Flexible'),
+        ('4', 'Per Project'),
+        ('5', 'Unknown'),
+    ]
+    wage_method = models.CharField(
+        max_length=1,
+        choices=WAGE_METHOD_CHOICES,
+        default='Hourly',
+    )
     wage = models.FloatField()
     date_of_start = models.DateField(max_length=8)
 
@@ -67,21 +74,13 @@ class Login_Details(models.Model):
 
 class WorkingHour(models.Model):
     id = models.AutoField(primary_key=True)
-    u_id=models.ForeignKey(User,on_delete=CASCADE)
-    working_date=models.DateField()
-    stime=models.TimeField()
-    etime=models.TimeField()
+    u_id = models.ForeignKey(User, on_delete=CASCADE)
+    working_date = models.DateField()
+    stime = models.TimeField()
+    etime = models.TimeField()
 
     def __str__(self):
-        return self.type
-
-
-class Status(models.Model):
-    id = models.IntegerField(primary_key=True)
-    type = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.type
+        return " | ".join([self.u_id, self.working_date, self.stime, self.etime])
 
 
 class Status(models.Model):
@@ -89,24 +88,24 @@ class Status(models.Model):
     desc = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.type
+        return "-".join([self.id, self.desc])
 
 
 class File(models.Model):
     id = models.AutoField(primary_key=True)
     urls = models.URLField(max_length=300)
-    file_creation_date=models.DateField()
+    file_creation_date = models.DateField()
 
     def __str__(self):
-        return self.type
+        return self.url+"-"+self.file_creation_date
 
 
 class Album(models.Model):
     id = models.AutoField(primary_key=True)
     album_name = models.CharField(max_length=100)
-    release_date=models.DateField()
-    size=models.IntegerField()
-    length=models.FloatField()
+    release_date = models.DateField()
+    size = models.IntegerField()
+    length = models.FloatField()
 
     def __str__(self):
         return "-".join([self.id, self.album_name])
@@ -115,42 +114,44 @@ class Album(models.Model):
 class Creation(models.Model):
     id = models.AutoField(primary_key=True)
     creator = models.CharField(max_length=100)
-    creation_date=models.DateField()
+    creation_date = models.DateField()
     creator = models.CharField(max_length=100)
-    album_id=models.ForeignKey(Album,on_delete=CASCADE)
-    current_file = models.ForeignKey(File, null=True, blank=True, on_delete=models.SET_NULL)
-    previous_file=models.ForeignKey(File, null=True, blank=True, on_delete=models.SET_NULL)
-    profit=models.FloatField()
+    album_id = models.ForeignKey(Album, on_delete=CASCADE)
+    current_file = models.ForeignKey(
+        File, null=True, blank=True, on_delete=models.SET_NULL, related_name='current_file')
+    previous_file = models.ForeignKey(
+        File, null=True, blank=True, on_delete=models.SET_NULL, related_name='previous_file')
+    profit = models.FloatField()
 
     def __str__(self):
-        return self.type
+        return self.id
 
 
 class Resource(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    unit_cost=models.FloatField()
+    unit_cost = models.FloatField()
 
     def __str__(self):
-        return self.type
+        return self.id
 
 
 class Phase(models.Model):
     id = models.AutoField(primary_key=True)
-    creation_id = models.ForeignKey(Creation,on_delete=CASCADE)
-    creation_id = models.ForeignKey(Creation,on_delete=CASCADE)
-    status=models.ForeignKey(Status,on_delete=models.SET_NULL)
-    placement=models.IntegerField()
-    name=models.CharField(max_length=100)
+    creation_id = models.ForeignKey(Creation, on_delete=CASCADE)
+    creation_id = models.ForeignKey(Creation, on_delete=CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    placement = models.IntegerField()
+    name = models.CharField(max_length=100)
     resources = models.ManyToManyField(Resource, through='Phase_Resources')
 
     def __str__(self):
-        return self.type
+        return self.id
 
 
 class Phase_Resources(models.Model):
-    phase = models.ForeignKey(Phase,on_delete=CASCADE)
-    resource = models.ForeignKey(Resource,on_delete=CASCADE)
+    phase = models.ForeignKey(Phase, on_delete=CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=CASCADE)
     resource_quantity = models.FloatField()
 
     def __str__(self):
@@ -159,22 +160,20 @@ class Phase_Resources(models.Model):
 
 class Meeting(models.Model):
     id = models.AutoField(primary_key=True)
-    phase_id=models.ForeignKey(Phase,on_delete=CASCADE)
-    attendees=models.ManyToManyField(User)
-    start_date=models.DateField()
-    end_date=models.DateField()
-    start_time=models.TimeField()
-    end_time=models.TimeField()
-    #https://djangotricks.blogspot.com/2019/10/working-with-dates-and-times-in-forms.html
-    
-
+    phase_id = models.ForeignKey(Phase, on_delete=CASCADE)
+    attendees = models.ManyToManyField(User)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    # https://djangotricks.blogspot.com/2019/10/working-with-dates-and-times-in-forms.html
 
     def __str__(self):
-        return self.type
+        return self.id
 
 
 class File_Deletion_History(models.Model):
     id = models.AutoField(primary_key=True)
-    creation_id = models.models.ForeignKey(Creation, on_delete=models.CASCADE)
-    phase_id = models.models.ForeignKey(Phase, on_delete=models.CASCADE)
+    creation_id = models.ForeignKey(Creation, on_delete=models.CASCADE)
+    phase_id = models.ForeignKey(Phase, on_delete=models.CASCADE)
     deletion_date = models.DateField()
