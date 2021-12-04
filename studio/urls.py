5bbd4f12e837938d582d394f4dc0ca7fe167ae6c
vlad21591 <vlad.census@gmail.com>
2021-12-04 14:36:05 +0200

    Start

---
+++ studio/urls.py
@@ -0,0 +1,32 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("",views.index),
+
+    #Admin
+    path("admin/",views.admin_page),
+    path("admin/add-client",views.admin_new_client),
+    path("admin/update-details",views.admin_update_detail),
+    path("admin/first-meeting",views.admin_first_meeting),
+    path("admin/manage-creations",views.admin_manage_creations),
+    path("admin/personnel-lists",views.admin_manage_personnel),
+    path("admin/meetings",views.admin_meetings_calendar),
+    path("admin/finance",views.admin_finance),
+    path("admin/last-files",views.admin_last_files),
+
+    #Worker
+    path("worker/",views.worker_page),
+    path("worker/tasks",views.worker_tasks),
+    path("worker/upload",views.worker_upload),
+    path("worker/mettings",views.worker_mettings),
+
+    #Client
+    path("client/",views.client_page),
+    path("client/creations",views.client_creations),
+    path("client/meetings",views.client_meetings),
+    path("client/details",views.client_details)
+
+]
--- studio/views.py
+++ studio/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,"studio/index.html")
+
+def admin_page(request):
+    pass
+
+def admin_new_client (request):
+    pass
+
+def admin_update_detail (request):
+    pass
+
+def admin_first_meeting(request):
+    pass
+
+def admin_manage_creations(request):
+    pass
+
+def admin_manage_personnel(request):
+    pass
+
+def admin_meetings_calendar (request):
+    pass
+
+def admin_finance(request):
+    pass
+
+def admin_last_files(request):
+    pass
+
+def worker_page(request):
+    pass
+
+def worker_tasks(request):
+    pass
+
+def worker_upload(request):
+    pass
+
+def worker_mettings(request):
+    pass
+
+def client_page(request):
+    pass
+
+def client_creations(request):
+    pass
+
+def client_meetings(request):
+    pass
+
+def client_details(request):
+    pass
