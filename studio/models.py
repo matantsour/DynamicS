fd533bb916d2ba812347746aaaafe199f19d25e6
Matan Tsour <matani.ts1@gmail.com>
2021-12-18 19:31:53 +0200

    final today hoping so

--- studio/models.py
+++ studio/models.py
@@ -184,11 +184,9 @@ class Meeting(models.Model):
 
 class File_Deletion_History(models.Model):
     id = models.AutoField(primary_key=True)
-    creation_id = models.ForeignKey(Creation, on_delete=models.CASCADE)
-    phase_id = models.ForeignKey(Phase, on_delete=models.CASCADE)
-    url = models.URLField(max_length=300, blank=True, null=False)
+    file_id = models.ForeignKey(File, on_delete=models.CASCADE)
     deletion_date = models.DateField(
         auto_now=True, editable=False, null=False, blank=True)
 
     def __str__(self):
-        return "-".join([str(self.id), str(self.creation_id.name), str(self.phase_id.name)])
+        return "-".join([str(self.id), str(self.file_id.url)])
