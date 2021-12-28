956453930e1d4e6a7b4eb474ae99906ae9ef8577
Matan Tsour <matani.ts1@gmail.com>
2021-12-28 17:14:29 +0200

    changes

--- studio/models.py
+++ studio/models.py
@@ -133,15 +133,7 @@ class Creation(models.Model):
             return 0
 
 
-class File(models.Model):
-    id = models.AutoField(primary_key=True)
-    url = models.URLField(max_length=300)
-    file_creation_date = models.DateField(
-        null=False, blank=True, editable=False, auto_now=True)
-    creation = models.ForeignKey(
-        Creation, null=True, blank=True, on_delete=models.SET_NULL)
-    def __str__(self):
-        return str(self.file_creation_date)+"|"+self.url
+
 
 
 
@@ -186,6 +178,15 @@ class Phase_Resources(models.Model):
     def __str__(self):
         return "-".join([str(self.phase.phase_id), self.resource.name, str(self.resource_quantity)])
 
+class File(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.URLField(max_length=300)
+    file_creation_date = models.DateField(
+        null=False, blank=True, editable=False, auto_now=True)
+    creation = models.ForeignKey(
+        Creation, null=True, blank=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return str(self.file_creation_date)+"|"+self.url
 
 class Meeting(models.Model):
     id = models.AutoField(primary_key=True)
@@ -207,7 +208,6 @@ class Meeting(models.Model):
 class File_Deletion_History(models.Model):
     id = models.AutoField(primary_key=True)
     creation_id = models.ForeignKey(Creation, on_delete=models.CASCADE)
-    phase_id = models.ForeignKey(Phase,to_field="phase_id", on_delete=models.CASCADE)
     url = models.URLField(max_length=300, blank=True, null=False)
     deletion_date = models.DateField(
         auto_now=True, editable=False, null=False, blank=True)
