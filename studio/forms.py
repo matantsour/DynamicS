7b9564f1a31ab323ed8f3b153fa2d38a510634f1
Matan Tsour <matani.ts1@gmail.com>
2022-02-20 11:15:23 +0200

    password_visibility_as_password

--- studio/forms.py
+++ studio/forms.py
@@ -1,10 +1,13 @@
 from django import forms
+
+
 class LoginForm(forms.Form):
     user_name = forms.CharField(max_length=200,
-    error_messages={
-        "required": "יש להזין שם משתמש/אימייל",
-        "max_length":"יש להזין שם קצר יותר"
-    })
-    passcode=forms.CharField(error_messages={
+                                error_messages={
+                                    "required": "יש להזין שם משתמש/אימייל",
+                                    "max_length": "יש להזין שם קצר יותר"
+                                })
+    passcode = forms.CharField(widget=forms.PasswordInput(),
+                               error_messages={
         "required": "יש להזין סיסמה"
     })
