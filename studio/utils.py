6f182628c721ef4d257e93edaa204d1774eac851
Matan Tsour <matani.ts1@gmail.com>
2022-02-20 11:11:59 +0200

    fix_

--- studio/utils.py
+++ studio/utils.py
@@ -1,1 +1,24 @@
 from .models import *
+
+SESSIONS_DEFALUTS = {"is_logged_in":False,
+                     "user_logged_in":False,
+                     "user_type":"guest",
+                     }
+
+#Views Utils
+def reset_sessions_to_default(request):
+    for key,default_val in SESSIONS_DEFALUTS.items():
+        request.session[key]=default_val
+
+def getUser(email, passcode):
+    query_results = Login_Details.objects.filter(
+        email=email, password=passcode)
+    if not query_results:
+        return None
+    else:
+        fetched_user = query_results[0].u_id
+        return fetched_user
+
+
+
+#Models Utils
--- studio/views.py
+++ studio/views.py
@@ -6,50 +6,13 @@ from django.conf import settings
 from .models import *
 from django.db.models import Q
 from .forms import LoginForm
-
-SESSIONS_DEFALUTS = {"is_logged_in":False,
-                     "user_logged_in":False,
-                     "user_type":"guest",
-                     }
-
-# Create your util functions here.
-
-def reset_sessions_to_default(request):
-    for key,default_val in SESSIONS_DEFALUTS.items():
-        request.session[key]=default_val
-
-def logoutFunc(request):
-    reset_sessions_to_default(request)
-    login_form = LoginForm()
-    return render(request, "studio/index.html", {"login_form": login_form})
+from .utils import *
 
 
-def all_unexpired_sessions_for_user(user):
-    user_sessions = []
-    all_sessions = Session.objects.filter(
-        expire_date__gte=datetime.datetime.now())
-    for session in all_sessions:
-        session_data = session.get_decoded()
-        if user.pk == session_data.get('_auth_user_id'):
-            user_sessions.append(session.pk)
-    return Session.objects.filter(pk__in=user_sessions)
+
+# Create your util functions here, then move them to utils.
 
 
-def delete_all_unexpired_sessions_for_user(user, session_to_omit=None):
-    session_list = all_unexpired_sessions_for_user(user)
-    if session_to_omit is not None:
-        session_list.exclude(session_key=session_to_omit.session_key)
-    session_list.delete()
-
-
-def getUser(email, passcode):
-    query_results = Login_Details.objects.filter(
-        email=email, password=passcode)
-    if not query_results:
-        return None
-    else:
-        fetched_user = query_results[0].u_id
-        return fetched_user
 
 
 # create your class views here
@@ -76,7 +39,15 @@ class indexView(View):
         return render(request, "studio/index.html", {"login_form": login_form})
 
 
-# previous views functions
+#functional views
+
+def logoutFunc(request):
+    reset_sessions_to_default(request)
+    login_form = LoginForm()
+    return render(request, "studio/index.html", {"login_form": login_form})
+
+# old views functions
+
 def index(request):
     return render(request, "studio/index.html")
 
