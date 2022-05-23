03450da4aad795bda91951005be9bd3b5023190f
Matan Tsour <matani.ts1@gmail.com>
2022-05-23 21:53:07 +0300

    UPLOAD_FILES_AUDIO_WORKS

--- studio/urls.py
+++ studio/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -71,3 +73,7 @@ urlpatterns = [
     path("client/details",views.client_details)
 
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
